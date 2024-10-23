92edd301436acadc64e51643d4374381c471cb5d
Marcello <marcello.faugiana@kyklos-group.com>
2024-10-23 09:58:57 +0200

    Added test t show memory bug in DistanceFromOrigin

--- ConsoleTests/OOPExamples.Tests/Point2DTests.cs
+++ ConsoleTests/OOPExamples.Tests/Point2DTests.cs
@@ -43,4 +43,62 @@ public class Point2DTests
         p1.DistanceFrom(p2).Should().Be(5d);
         p2.DistanceFrom(p1).Should().Be(5d);
     }
+
+    private Point2D[] CreateRandomPoints(int n)
+    {
+        Random rnd = new Random();
+
+        double GetNextXY()
+        {
+            int sign = rnd.Next(2) == 0 ? 1 : -1;
+            return sign * 100 * rnd.NextDouble();
+        }
+
+        double xrnd = 100 * rnd.NextDouble();
+
+        return
+            Enumerable.Range(0, n)
+            .Select
+            (
+                _ => new Point2D(GetNextXY(), GetNextXY())
+            )
+            .ToArray();
+
+        // Equivalente del LinQ precedente. Versione con List<T>
+        //List<Point2D> listOfPoints = new List<Point2D>();
+        //for (int i = 0; i < n; i++)
+        //{
+        //    Point2D p = new Point2D(GetNextXY(), GetNextXY());
+        //    listOfPoints.Add(p);
+        //}
+        //Point2D[] points = listOfPoints.ToArray();
+
+
+        // Equivalente del LinQ precedente. Versione con array
+        //Point2D[] points = new Point2D[n];
+
+        //for (int i = 0; i < n; i++)
+        //{
+        //    Point2D p = new Point2D(GetNextXY(), GetNextXY());
+        //    points[i] = p;
+        //}
+    }
+
+
+    [Fact]
+    public void Test_tbd()
+    {
+        Point2D[] points = CreateRandomPoints(10_000);
+
+        int count = 100_000;
+
+        for (int i = 0; i < count; i++)
+        {
+            for (int j = 0; j < points.Length; j++)
+            {
+                double distance = points[j].DistanceFromOrigin();
+            }
+        }
+    }
+
 }
